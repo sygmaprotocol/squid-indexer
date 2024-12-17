c6b164d16ce6dfc8738e1e3e8b0a4f64a5d804e7
mj52951 <matija.jelavic@chainsafe.io>
2024-12-17 11:08:23 +0100

    test

--- start-prod.sh
+++ start-prod.sh
@@ -3,7 +3,7 @@
 if [ "$START_SCRIPT_ENV" = "index" ]; then
   node ./lib/main.js
 elif [ "$START_SCRIPT_ENV" = "init" ]; then
-  sqd migration:apply && node ./lib/main_init.js
+  sqd migration:generate && node ./lib/main_init.js
 else
   exit 1
 fi
